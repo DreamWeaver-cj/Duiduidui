a88209ab83651fb6c25dd4940e42a2d622a73586
用户名 <1156348816@qq.com>
2020-04-05 11:44:52 +0800

    封装类2

---
+++ CONNECT/FindLoop.py
@@ -0,0 +1,75 @@
+# NAME：ChenJing
+# DATE：202004041121
+# FUNC：查找环
+# QSTN：暂无
+
+import numpy as np
+
+'''
+# 寻找循环路径
+# index 代表流动到第index家公司了，index最大为7，也就是最多流动7家公司
+# road 为未添加转账记录的路径
+# res 为存储环的集合
+# road_end 为未添加转账记录的最后一个记录的ID1
+
+# 输入二维数组
+# 输出环的个数，并将环存在txt中
+'''
+class FindLoop():
+    def run(self, ds=np.genfromtxt('../dataset/test_cy/10.txt', delimiter=',', dtype=int)[:, :-1]):
+        print('run.')
+        self.ds = ds
+        res = []  # 代表存所有路径的数组
+        road = []
+        loop_road = []  # 代表资金流动成环了A->B->C->A,也就是洗钱了
+
+        # 程序从这里开始执行循环，遍历整个数组，如果第二个值的ID1==第一个值的ID2，进入递归函数
+        for loop_start in range(ds.shape[0]):
+            for loop_second in range(ds.shape[0]):
+                if ds[loop_second][0] == ds[loop_start][1]:
+                    del road[:]  # 代表资金流动路径，A->B->C公司转账则为[A,B,C]
+                    road.append(ds[loop_start][0])
+                    road.append(ds[loop_start][1])
+
+                    # print('第一层循环' + str(i), str(j))
+
+                    self.loop(road, res, 3, loop_second)
+                    road.pop()
+        print('finished.')
+        fh = open('../res/result_cj_backtrack04051000.txt', 'w', encoding='utf-8')  # a 是追加的意思
+        fh.write(str(len(set(res))) + '\n')
+        fh = open('../res/result_backtrack04051000.txt', 'a', encoding='utf-8')  # a 是追加的意思
+        for i in set(res):
+            fh.write(i + '\n')
+        fh.close()
+        return str(len(set(res)))
+
+    def loop(self, road, res, index, i):
+        # 如果路径超过7，不再递归
+        if index > 7:
+            road.append(-1)
+            return
+
+            # 循环查找
+        for j in range(self.ds.shape[0]):
+            # 如果找到转账的下一家公司
+            if self.ds[j][0] == self.ds[i][1] and self.ds[j][0] not in road:
+                road.append(self.ds[j][0])
+
+                if self.ds[j][1] == road[0] and len(road) < 8:
+                    loop_road = road
+                    while loop_road[0] != min(loop_road):
+                        loop_road.insert(0, loop_road[-1])
+                        loop_road.pop()
+
+                    res.append(','.join(str(num) for num in loop_road))
+                    return
+
+                # 继续寻找钱款流向的下一家公司
+                self.loop(road, res, index + 1, j)
+                road.pop()
+
+
+if __name__ == '__main__':
+    obj = FindLoop()
+    obj.run()
--- cy/5.py
+++ cy/5.py
@@ -1,12 +1,13 @@
 # NAME：ChengYu
-# DATE：20200402-2
+# DATE：20200404-2
 # FUNC：字典查找网络全部元素
 # QSTN：网络个数不能过大，否则会爆内存；
-#       只能大致过滤网孔，网眼计算只是估算不过可以看是一个环还是一个网，可能会有多余的线（只多不少，少的情况还没出现过）
+#       更改了“网”的定义：包含所有互通元素的集合，未检测bug，官方用例pass
+#       eye是网眼个数估计值，不准确，但能确定网络是否只有一个环
 
 import numpy as np
 import time
-
+import Global
 
 #列表转字典(lst:被转换数据，dic:输出字典，n:数据总行数)
 def Lst2Dic(lst:[]):
@@ -22,40 +23,42 @@ def Lst2Dic(lst:[]):
 #查找网络（dic：要查找的字典，ID，开始查询的位置）
 def Find_Net(dic:{},ID:int):
     # 查找链(dic:要查找的字典,ID:查找字典中的ID，tmp:传入一个含ID的列表贮存链,RES：保存网络用于迭代的传入，eye:网孔数量)
-    def Find_chain(dic: {}, ID: int, tmp: [], RES: [], eye: int):
+    def Find_chain(dic: {}, ID: int):
         if (ID in dic):         #判断传进来的字典里有没有
             n = len(dic[ID])  # ID1发送的用户有多少
             # 递归结束条件：ID不在字典中，链超过7
             for ID0 in dic[ID]:  # 遍历ID1发送的用户
-                if len(tmp)-1 <= 100:  # 如果链的长度不超标（不起限制作用）
+                if len(Global.chain)-1 <= 100:  # 如果链的长度不超标（不起限制作用）
                     if ID0 in dic:  # 如果此用户作为发送人在字典中存在
 
-                        if (ID0 not in tmp[:-1] and ID0 not in RES):  # 如果没闭合成环
-                            tmp += [ID0]  # 存在缓存里
-                            RES, tmp, eye = Find_chain(dic, ID0, tmp, RES,eye)  # 递归
+                        if (ID0 not in Global.chain[:-1] and ID0 not in Global.net):  # 如果没闭合成环
+                            Global.chain += [ID0]  # 存在缓存里
+                            Find_chain(dic, ID0)  # 递归
 
                         else: #闭合成环了
-                            if len(RES) == 0:  # 如果还没找到环
-                                del tmp[:tmp.index(ID0)]  # 截断环前面的链
 
-                            RES += tmp  # 把这个数组接到后面
-                            eye += 1  # 网孔加一
-                            tmp = []  # 删除tmp保存的环，就是转移到RES里面，然后存新环了
+                            Global.net += Global.chain  # 把这个数组接到后面
+                            Global.eye += 1  # 网孔加一
+                            Global.chain = []  # 删除tmp保存的环，就是转移到RES里面，然后存新环了
 
                     else:  # 链断了
                         continue
                 else:  # 超出n个了
                     continue
 
-            if len(tmp)!=0:#全遍子树历完了还没有就删除这个父节点
-                del tmp[-1]
-            return RES, tmp, eye
+            if len(Global.chain)!=0:#全遍子树历完了还没有就删除这个父节点
+                del Global.chain[-1]
+            return
 
         else:
-            return RES, tmp, 0
+            return
 ####################################################
-    net,drop,flag=Find_chain(dic, ID, [ID], [],0)
-    return net,flag
+
+    Global.eye=0
+    Global.net=[ID]
+    Global.chain=[]
+    Find_chain(dic, ID)
+    return Global.net,Global.eye
 
 #求网络总和（net:网络）
 def Net_Sum(net:[]):
@@ -84,34 +87,40 @@ def Delet(dic:{},net:[]):
         del dic[ID]
     return dic
 
+#对接函数（dic:查找的字典,ls：网络列表）
+def Connect(dic:{},ls:[]):
+    res=[]
+    for ID0 in ls:
+        for ID1 in dic[ID0]:
+            if ID1 in ls:
+                res.append([ID0,ID1])
+    return res
 
+#网络查找接口函数（dic：查找的字典）
+def FIND_NET(dic:{}):
+    res=[[]]
+    net, eye = Find_Net(dic, ID)  # 查找网
+    if len(net) != 0:  # 如果成网络了
+        res = Connect(dic, net)
+    else:  # 节点没有网络连接
+        del dic[ID]  # 删掉节点
+    for ID0 in net:  #
+        del dic[ID0]
+
+    return res,eye
 
 if __name__=='__main__':
 
     start1 = time.perf_counter()
+    Global.dic = Read_Data("../dataset/test_data.txt")# 读取文件保存成字典
 
-    dic = {}  # 存储字典（ID1:[ID2]）
-    Find = []#保存各个网络的和用于判定是否查找过
-    aaa=0#求网眼的和
-
-    dic = Read_Data(".//ds//test_data.txt")# 读取文件保存成字典
-
-    # net, flag = Find_Net(dic, 2250)  # 查找链
-    # print(net, '|', flag)  # 打印网
-
-    for ID in dic:
-        net,eye=Find_Net(dic,ID)#查找链
-        if not IsIllegal(net,Find,eye):#如果合法
-            Find+=Net_Sum(net)#查找是否查找过这个网
-            aaa+=eye#网眼求和
-            print(eye,net)  # 打印网
-        else:
-            continue
-    print('eye=',aaa)
-
-
+    while (len(Global.dic) != 0):
+        for ID in Global.dic:
+            net,eye=FIND_NET(Global.dic)
+            if (len(net)!=0):
+                print(eye,net)
+            break
     end1 = time.perf_counter()
-
     print("final is in : %s Seconds " % (end1 - start1))
 
 
